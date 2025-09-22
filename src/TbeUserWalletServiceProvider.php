1393b1f8b290eb2b70f46dcb805edffeffd2c6d2
Elyar <elyareranjbar@gmail.com>
2025-09-22 19:37:22 +0330

    Feature registrations

--- src/TbeUserWalletServiceProvider.php
+++ src/TbeUserWalletServiceProvider.php
@@ -5,6 +5,9 @@ namespace TelegramBotEssentials\UserWallet;
 use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Support\ServiceProvider;
 use TelegramBotEssentials\Essence\Exceptions\LogicException;
+use TelegramBotEssentials\Essence\Telegram\StateAnswers\Member\MyWalletAnswer;
+use TelegramBotEssentials\UserWallet\Telegram\CallbackQueries\Member\MyWalletQuery;
+use TelegramBotEssentials\UserWallet\Telegram\ReplyKeys\Member\MyWalletKey;
 
 class TbeUserWalletServiceProvider extends ServiceProvider
 {
@@ -15,15 +18,15 @@ class TbeUserWalletServiceProvider extends ServiceProvider
     public function register(): void
     {
         replyKeyBus()->addReplyKeys([
-
+            MyWalletKey::class,
         ]);
 
         callbackQueryBus()->addCallbackQueries([
-
+            MyWalletQuery::class
         ]);
 
         stateAnswerBus()->addStateAnswers([
-
+            MyWalletAnswer::class
         ]);
     }
 }
